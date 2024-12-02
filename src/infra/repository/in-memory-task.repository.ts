313783ed54ea36ae65d8e16c027a0acf930e7006
Romain BOURREE <romain.bourree@immo-sign.com>
2024-12-02 14:57:17 +0100

    Add prisma

--- src/infra/repository/in-memory-task.repository.ts
+++ src/infra/repository/in-memory-task.repository.ts
@@ -1,24 +1,35 @@
+import { PrismaClient } from '@prisma/client';
 import { TaskRepository } from "src/domain/repositories/task.repository";
 import { Task } from "src/domain/entities/task.entity";
 
 // Here prisma call to database
-export class InMemoryTaskRepository implements TaskRepository {
-    private tasks: Map<string, Task> = new Map();
+export class PrismaTaskRepository implements TaskRepository {
+    private prisma = new PrismaClient();
 
     async save(task: Task): Promise<void> {
-        this.tasks.set(task.getId(), task);
+        await this.prisma.task.create({
+            data: {
+                id: task.getId(),
+                title: task.getTitle(),
+                description: task.getDescription()
+            }
+        })
     }
 
     async findById(id: string): Promise<Task | null> {
-        return this.tasks.get(id) || null;
+        const task = await this.prisma.task.findUnique({ where: { id } });
+        return task ? new Task(task.id, task.title, task.description, task.completed) : null;
     }
 
     async delete(id: string): Promise<void> {
-        this.tasks.delete(id);
+        await this.prisma.task.delete({ where: { id } });
     }
 
     async findAll(): Promise<Task[]> {
-        return Array.from(this.tasks.values());
+        const tasks = await this.prisma.task.findMany();
+        return tasks.map(
+            (task) => new Task(task.id, task.title, task.description, task.completed),
+        );
     }
 
 }
